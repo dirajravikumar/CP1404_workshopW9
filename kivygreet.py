0181ed48817b7bcc25f5d4d0dd60f4e435348ff9
Diraj Ravikumar <dirajravikumar@my.jcu.edu.au>
2016-05-24 12:37:57 +0800

    Added a few changes.

--- kivygreet.py
+++ kivygreet.py
@@ -1,18 +1,30 @@
 from kivy.app import App
 from kivy.lang import Builder
 
-
 class BoxLayoutDemo(App):
     def build(self):
+        """
+        Has the title of the file
+        Loads the kivygreet.kv
+        """
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('kivygreet.kv')
         return self.root
 
     def handle_greet(self):
+        """
+        When the user clicks the greet button, it prints out Hello, name in the label.
+        """
         self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
 
     def handle_clear(self):
+        """
+        When the user clicks clear button, it clears the text field and the label.
+        """
         self.root.ids.input_name.text = ''
         self.root.ids.output_label.text = ''
 
+"""
+Runs the BoxLayoutDemo class which loads kivy.app
+"""
 BoxLayoutDemo().run()
--- miletokilometer.py
+++ miletokilometer.py
@@ -1,14 +1,12 @@
 from kivy.app import App
 from kivy.lang import Builder
 
-
 MILES_TO_KM = 1.60934
 
 
 class MilesConverterApp(App):
-    """ MilesConverterApp is a Kivy App for converting miles to kilometres """
+    """ A kivy app that converts miles to kilometers"""
     def build(self):
-        """ build the Kivy app from the kv file """
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('miletokilometer.kv')
         return self.root
@@ -30,8 +28,7 @@ class MilesConverterApp(App):
 
     def get_validated_miles(self):
         """
-        get text input from text entry widget, convert to float
-        :return: 0 if error, float version of text if valid
+
         """
         try:
             value = float(self.root.ids.input_miles.text)
